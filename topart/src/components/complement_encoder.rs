ef60ee104869517ba11c12cb82c6b4e02e6adbad
Garibaldi <chanokin@gmail.com>
2025-03-04 17:07:45 +0000

    rusting it up

---
+++ topart/src/components/complement_encoder.rs
@@ -0,0 +1,8 @@
+use burn::{
+    nn::{
+        conv::{Conv2d, Conv2dConfig},
+        pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
+        Dropout, DropoutConfig, Linear, LinearConfig, Relu,
+    },
+    prelude::*,
+};
