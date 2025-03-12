4f5ffdec29d31fdf86ad1f4dfb7481de0c4456ea
chanokin <chanokin@gmail.com>
2025-03-12 15:33:43 +0000

    no batch-it crazy

--- topart/src/components/complement_encoder.rs
+++ topart/src/components/complement_encoder.rs
@@ -1,7 +1,7 @@
 use burn::prelude::*;
 /// ComplementEncoder
 /// I'm assuming the input has values between 0 and 1
-/// I'll start with the 1D case
+/// I'll start with the batch-less 1D case
 #[derive(Module, Clone, Debug, Default)]
 pub struct ComplementEncoder;
 
@@ -12,7 +12,7 @@ impl ComplementEncoder {
 
     // in the forward pass we'll create the new tensor with the complement of the input tensor
     // remember the shape is [batch_size, n_neurons]
-    pub fn forward<B: Backend>(&self, input: Tensor<B, 2>) -> Tensor<B, 2> {
+    pub fn forward<B: Backend>(&self, input: Tensor<B, 1>) -> Tensor<B, 1> {
         // note the -input + 1.0 is a weird way of computing the complement of the input tensor
         Tensor::cat(vec![input.clone(), -input.clone() + 1.0], 1)
     }
@@ -30,17 +30,16 @@ mod tests {
     #[test]
     fn num_elements() {
         // let sample = Tensor::<burn::backend::Wgpu<f32, i32>, 4>::zeros([1, 3, 4, 5], &Default::default());
-        let sample = TestTensor::<2>::zeros([4, 3], &Default::default());
-        let [batch_size, n_neurons] = sample.dims();
+        let sample = TestTensor::<1>::zeros([4], &Default::default());
+        let [n_neurons] = sample.dims();
         let sample_n_elements = sample.shape().num_elements();
-        assert_eq!(sample_n_elements, 12);
+        assert_eq!(sample_n_elements, 4);
 
         let encoder = ComplementEncoder::new();
         let output = encoder.forward(sample);
         assert_eq!(output.shape().num_elements(), 2 * sample_n_elements);
 
-        let [batch_size_i, n_neurons_i] = output.dims();
-        assert_eq!(batch_size, batch_size_i);
+        let [n_neurons_i] = output.dims();
         assert_eq!(n_neurons * 2, n_neurons_i);
     }
 }
--- topart/src/components/fuzzy_art.rs
+++ topart/src/components/fuzzy_art.rs
@@ -2,19 +2,25 @@ use burn::prelude::*;
 
 /// FuzzyIntersection
 /// basically an alias to the min_pair function
-pub fn fuzzy_intersection<B: Backend, const D: usize>(input_0: Tensor<B, D>, input_1: Tensor<B, 1>) -> Tensor<B, D> {
+pub fn fuzzy_intersection<B: Backend, const D: usize>(input_0: Tensor<B, D>, input_1: Tensor<B, D>) -> Tensor<B, D> {
     input_0.min_pair(input_1)
 }
 
 /// TaxicabNorm AKA Manhattan norm/L1 norm
-pub fn taxicab_norm<B: Backend>(input: Tensor<B, 2>) -> Tensor<B, 1> {
-    input.abs().sum_dim(1).squeeze(1)
+pub fn taxicab_norm<B: Backend>(input: Tensor<B, 1>) -> Tensor<B, 1> {
+    input.abs().sum_dim(0)
 }
 
-pub fn choice<B: Backend>(alpha: Float, input: Tensor<B, 2>, weights_i: Tensor<B, 1>) -> Tensor<B, 1> {
+/// Choice
+/// This is the choice function for the Fuzzy ART algorithm
+/// It takes the input tensor, the weights tensor, and the vigilance parameter alpha
+/// It returns the choice tensor which is the "fitness" of an output neuron to the input x weights
+pub fn choice<B: Backend, S: ElementConversion>(alpha: S, input: Tensor<B, 1>, weights_i: Tensor<B, 1>) -> Tensor<B, 1> {
 
-    let intersect = fuzzy_intersection(input, weights_i);
-    let norm = taxicab_norm(intersect);
+    let intersect = fuzzy_intersection(input, weights_i.clone());
+    let inter_norm = taxicab_norm(intersect);
+    let weights_norm = taxicab_norm(weights_i);
+    inter_norm / (weights_norm + alpha)
 }
 
 
@@ -27,12 +33,21 @@ mod tests {
 
 
     #[test]
-    fn num_elements() {
+    fn intersection() {
         let device = Default::default();
         let tensor1 = TestTensor::<1>::from_data([1.0, -2.0, 5.0], &device);
         let tensor2 = TestTensor::<1>::from_data([2.0, 3.0, 3.0], &device);
         let tensor = fuzzy_intersection(tensor1, tensor2);
         let expected_tensor = TestTensor::<1>::from_data([1.0, -2.0, 3.0], &device);
         assert!(expected_tensor.all_close(tensor, None, None));
+    }
+
+    #[test]
+    fn norm() {
+        let device = Default::default();
+        let tensor = TestTensor::<1>::from_data([1.0, -2.0, 3.0], &device);
+        let norm = taxicab_norm(tensor);
+        let expected_norm = TestTensor::<1>::from_data([6.0], &device);
+        assert!(expected_norm.all_close(norm, None, None));
     }
 }
